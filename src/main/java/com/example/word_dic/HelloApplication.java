809fa4bc225b23ed168e2cdab75f1648d0112111
Umesh8142 <102941752+Umesh8142@users.noreply.github.com>
2022-12-17 11:17:52 +0530

    create Word dictionary

--- src/main/java/com/example/word_dic/HelloApplication.java
+++ src/main/java/com/example/word_dic/HelloApplication.java
@@ -20,6 +20,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
+        
         root=new Group();
         stage.setTitle("Word Dictionary");
 
